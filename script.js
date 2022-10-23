c3a056ffb45f64990b01d22c32a2254ebc4a13cc
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2022-10-23 19:03:42 +0200

    Add img display feature based on mistake nums
    Add the feature to display the image according to the number of mistakes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const imgParts = ['rope', 'hanger', 'rod', 'floor', 'head', 'trunk', 'armL', 'armR', 'legL', 'legR']
+let mistakes = 0;
+
+
+for (let i = 0; i < imgParts.length; i++) {
+    document.querySelector(`.${imgParts[i]}`).classList.add('hide');
+}
+
+
+const btn = document.querySelector('.button');
+
+function display(){
+    document.querySelector(`.${imgParts[mistakes]}`).classList.remove('hide');
+    mistakes++;
+}
+
+btn.addEventListener('click', display);
